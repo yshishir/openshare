95178102a9f4dbe195d947304c803a0a718d747d
Shishir <shishirtech506@gmail.com>
2026-07-15 18:43:58 +0530

    setup font and all

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      Hello
+    </div>
   );
 }
