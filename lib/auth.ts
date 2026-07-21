da133b464544e8dfe831d038e98e460c405b64c7
Shishir <shishirtech506@gmail.com>
2026-07-21 19:50:37 +0530

    google oauth setup:

--- lib/auth.ts
+++ lib/auth.ts
@@ -13,4 +13,10 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
 });
