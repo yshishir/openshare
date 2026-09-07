fda488a68b8f3d7868c114b451a4b4f076b61d7a
Shishir <shishirtech506@gmail.com>
2026-09-08 03:58:10 +0530

    share api

--- app/api/shares/route.ts
+++ app/api/shares/route.ts
@@ -1,20 +1,61 @@
 import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
+import { randomUUID } from "crypto";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-    const session = await auth.api.getSession({
-        headers: await headers(),
-    });
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
-    if(!session) {
-        return NextResponse.json({ error: "Not Authorized" }, { status: 401 });
-    }
-    const body = await req.json();
-    const { files, password } = body;
+  if (!session) {
+    return NextResponse.json(
+      { error: "Not authorized" },
+      { status: 401 },
+    );
+  }
 
-    let passwordHash = null;
-    passwordHash = await bcrypt.hash(password.trim(),10);
+  const { files, password } = await req.json();
 
+  if (!files || files.length === 0) {
+    return NextResponse.json(
+      { error: "Please upload at least one file" },
+      { status: 400 },
+    );
+  }
+
+  const passwordHash = password
+    ? await bcrypt.hash(password, 10)
+    : null;
+
+  const share = await prisma.share.create({
+    data: {
+      shareToken: randomUUID(),
+      userId: session.user.id,
+      passwordHash,
+      expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000),
+
+      files: {
+        create: files.map((file: {
+          name: string;
+          size: number;
+          type: string;
+          publicId: string;
+          url: string;
+        }) => ({
+          name: file.name,
+          size: file.size,
+          type: file.type,
+          cloudinaryPublicId: file.publicId,
+          cloudinaryUrl: file.url,
+        })),
+      },
+    },
+  });
+
+  return NextResponse.json({
+    shareToken: share.shareToken,
+  });
 }
